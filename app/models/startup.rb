3a99b9334e87ba1f6452372b305e319b16a7562b
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-29 01:54:51 +0000

    Automatically backed up by Learn

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,37 @@
-class Startup
+class FundingRound
 
+    attr_reader :startup, :venture_capitalist, :type, :investment
+    @@all = []
+
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+
+        if investment < 0
+            @investment = 0
+        else
+            @investment = investment.to_f
+        end
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.find_by_founder(name)
+        self.all.find do |startup|
+            startup.founder == name
+        end
+    end
+
+    def self.domains
+        self.all.map do |startup|
+            startup.domain
+        end
+    end
+
+end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,18 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+s1 = Startup.new("twitter", "Jack", "twitter.com")
+s2 = Startup.new("facebook", "Mark", "facebook.com")
+s3 = Startup.new("reddit", "Satoshi", "reddit.com")
+
+# name, total_worth
+v1 = VentureCapitalist.new("Dan", 2_000_000)
+v2 = VentureCapitalist.new("Ali", 100_000_000)
+v3 = VentureCapitalist.new("zok", 6_0000_000_000_000)
+
+# startup, venture_capitalist, type, investment
+f1 = FundingRound.new(s1, v1, "seed", 500000)
+f2 = FundingRound.new(s1, v2, "seed", 900)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
